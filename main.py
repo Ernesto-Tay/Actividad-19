f82f98b945f51b65680263195a804cfd86ac2781
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-13 13:48:08 -0600

    Opciones 1 y 2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class lista:
         self.lista.append(item)
     def mostrar(self, item):
         print(f"Nombre: {item.nombre}")
-        print(f"Precio: {item.precio}")
+        print(f"Precio: Q{item.precio}")
         print(f"Peso: {item.peso}")
         print(f"Tipo: {item.tipo}")
 
@@ -25,41 +25,35 @@ class Galleta:
 
     def validar(self):
         while True:
-            nombre_ok = True
-            peso_ok = True
-            precio_ok = True
-            if self.nombre.len()<=1:
-                print("El nombre debe tener un largo mínimo de 3 letras")
-                nombre_ok = False
-            if self.precio <= 0:
-                print("El precio debe ser mayor a cero")
-                peso_ok = False
-            if self.peso <= 0:
-                print("El peso debe ser mayor a cero")
-                peso_ok = False
+            try:
+                self.nombre = input("\nIngrese el nombre de la galleta: ")
+                self.precio = int(input("Ingrese el precio del galleta: "))
+                self.peso = int(input("Ingrese el peso del galleta: "))
+                if self.nombre.len()<3:
+                    raise ValueError("El nombre debe tener al menos 3 letras")
+                if self.precio <= 0:
+                    raise ValueError("El precio debe ser mayor a 0")
+                if self.peso <= 0:
+                    raise ValueError("El peso debe ser mayor a 0")
 
-            if nombre_ok and peso_ok and precio_ok:
-                break
-            else:
-                while True:
-                    try:
-                        self.nombre = input("\nIngrese el nombre de la galleta: ")
-                        self.precio = int(input("Ingrese el precio del galleta: "))
-                        self.peso = int(input("Ingrese el peso del galleta: "))
-                        break
-                    except ValueError:
-                        print("Las entradas numéricas deben ser números enteros")
-                    except Exception as e:
-                        print("Error inesperado: ",e)
+            except ValueError:
+                print("Las entradas numéricas deben ser números enteros")
+            except Exception as e:
+                print("Error inesperado: ",e)
 
     def mostrar_info(self):
-        for i in main_lista.lista:
-            main_lista.mostrar(i)
+        if not main_lista.lista:
+            print("No hay galletas en la lista")
+        else:
+            for i in main_lista.lista:
+                main_lista.mostrar(i)
+                print(f"Tipo: {i.tipo}")
 
 
 class Relleno:
     def __init__(self,sabor_relleno):
         self.sabor_relleno = sabor_relleno
+
 
     def describir_relleno(self):
         descripciones = [f"Un exquisito sabor a {self.sabor_relleno}", f"Una cremosa mezcla que sabe a {self.sabor_relleno}", f"Una explosión de {self.sabor_relleno} que te deja deseando más"]
@@ -71,6 +65,28 @@ class GalletaChispas(Galleta):
         super().__init__(nombre,precio,peso)
         self.cantidad_chispas = cantidad_chispas
         self.tipo = "chispas"
+
+    def validar(self):
+        while True:
+            try:
+                self.nombre = input("\nIngrese el nombre de la galleta: ")
+                self.precio = int(input("Ingrese el precio del galleta: "))
+                self.peso = int(input("Ingrese el peso del galleta: "))
+                self.cantidad_chispas = int(input("Ingrese la cantidad de chispas: "))
+                if self.nombre.len()<3:
+                    raise ValueError("El nombre debe tener al menos 3 letras")
+                if self.precio <= 0:
+                    raise ValueError("El precio debe ser mayor a 0")
+                if self.peso <= 0:
+                    raise ValueError("El peso debe ser mayor a 0")
+                if self.cantidad_chispas <= 0:
+                    raise ValueError("La cantidad de chispas debe ser mayor a 0")
+
+            except ValueError:
+                print("Las entradas numéricas deben ser números enteros")
+            except Exception as e:
+                print("Error inesperado: ",e)
+
 
     def mostrar_info(self):
         for i in main_lista.lista:
@@ -84,6 +100,28 @@ class GalletaRellena(Galleta, Relleno):
         self.sabor_relleno = sabor_relleno
         self.tipo = "relleno"
 
+    def validar(self):
+        while True:
+            try:
+                self.nombre = input("\nIngrese el nombre de la galleta: ")
+                self.precio = int(input("Ingrese el precio del galleta: "))
+                self.peso = int(input("Ingrese el peso del galleta: "))
+                self.sabor_relleno = input("Ingrese el tipo de relleno: ")
+                if self.nombre.len()<3:
+                    raise ValueError("El nombre debe tener al menos 3 letras")
+                if self.precio <= 0:
+                    raise ValueError("El precio debe ser mayor a 0")
+                if self.peso <= 0:
+                    raise ValueError("El peso debe ser mayor a 0")
+                if not self.sabor_relleno:
+                    raise ValueError("Debe ingresar un nombre de relleno")
+
+            except ValueError:
+                print("Las entradas numéricas deben ser números enteros")
+            except Exception as e:
+                print("Error inesperado: ",e)
+
+
     def mostrar_info(self):
         for i in main_lista.lista:
             if i.tipo == "relleno":
@@ -94,9 +132,15 @@ while True:
     select = input("Seleccione una opción: ")
     match select:
         case "1":
-            pass
+            galleta = Galleta("",0,0)
+            galleta.validar()
+            main_lista.agregar(galleta)
+
         case "2":
-            pass
+            galletaChispada = GalletaChispas("","","",0)
+            galletaChispada.validar()
+            main_lista.agregar(galletaChispada)
+
         case "3":
             pass
         case "4":
